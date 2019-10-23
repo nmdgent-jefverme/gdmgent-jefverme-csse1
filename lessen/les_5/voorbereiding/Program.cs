f98c5123d687e7f5a0750c8032c0656b280ba806
nmdgent-jefverme <jefvermeireown@gmail.com>
2019-10-23 16:42:01 +0200

    voorbereiding gemaakt

---
+++ lessen/les_5/voorbereiding/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace voorbereiding
+{
+    class Program
+    {
+        private const int getal1 = 8;
+        private const int getal2 = 3;
+        static int optellen(int a = getal1, int b = getal2){
+            return a + b;
+        }
+        static int aftrekken(int a = getal1, int b = getal2){
+            return a - b;
+        }
+        static int vermenigvuldigen(int a = getal1, int b = getal2){
+            return a * b;
+        }
+        static double delen(int a = getal1, int b = getal2){
+            return a / b;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(getal1 + " * " + getal2 + " = " + vermenigvuldigen());
+            Console.WriteLine(getal1 + " / " + getal2 + " = " + delen());
+            Console.WriteLine(getal1 + " + " + getal2 + " = " + optellen());
+            Console.WriteLine(getal1 + " - " + getal2 + " = " + aftrekken());
+        }
+    }
+}
