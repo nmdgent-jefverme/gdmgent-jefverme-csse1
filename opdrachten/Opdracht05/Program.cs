df8d3b008a9ea580a7a0d99d6be8198068e5951b
nmdgent-jefverme <jefvermeireown@gmail.com>
2019-11-06 17:02:18 +0100

    finished opdrach05

--- opdrachten/Opdracht05/Program.cs
+++ opdrachten/Opdracht05/Program.cs
@@ -96,7 +96,7 @@ namespace Opdracht05
             string fullName = "";
             if(role.ToLower() == "docent"){
                 if (fName.Length < 4){
-                    fullName += fName.ToLower();
+                    fullName += fName.ToLower() + lName.ToLower().Substring(0, 2);
                 } else {
                     fullName += fName.ToLower().Substring(0, 4) + lName.ToLower().Substring(0, 2);
                 }
@@ -113,43 +113,144 @@ namespace Opdracht05
         }
         static void CheckIban(string iban){
             string str = iban.Replace(" ", "");
-            SchrijfLog(str.Length);
-            if (str.Length != 16){
-                throw new System.ArgumentException("De lengte van een iban moet precies 16 karakters zijn.");
+            string land = str.Substring(0, 2).ToLower();
+            string tmp = "";
+            foreach (char c in land)
+            {
+                tmp+= (int) c - 87;
             }
+            tmp += str.Substring(2,2);
             string numbers = str.Substring(4);
-            double modulo = Modulo(Convert.ToDouble(numbers), 97.0);
-            SchrijfLog(modulo);
+            numbers += tmp;
+            long mod = Convert.ToInt64(numbers) % 97;
+            if (str.Length != 16 || land != "be" || mod != 1){
+                throw new System.ArgumentException("Dit is geen geldige IBAN!");
+            } else {
+                SchrijfLog(iban + " is een geldige IBAN.");
+            }
+        }
+        static int Faculteit(int lengte){
+            int faculteit = lengte;
+            int a = 1;
+            for (int i = 1; i <= faculteit; i++)
+            {
+                a = a * i;
+            }
+            return a;
+        }
+        static string Fibonacci(int aantal){
+            int a = 0;
+            int b = 1;
+            string fibonaciRow = "0";
+            for (int i = 1; i < aantal; i++)
+            {
+                fibonaciRow += ", ";
+                int temp = a;
+                a = b;
+                fibonaciRow += Convert.ToString(a);
+                b = temp + b;
+            }
+            return fibonaciRow;
+        }
+        static void RunMenu(){
+            SchrijfLog("Welke functie wil u graag testen? Dit zijn de opties:");
+            SchrijfLog("1. print de wiskundige functies op het scherm.");
+            SchrijfLog("2. Een willekeurig nummer ophalen.");
+            SchrijfLog("3. Een willekeurig nummer ophalen binnen een bepaald bereik.");
+            SchrijfLog("4. De lottotrekking bekijken.");
+            SchrijfLog("5. De euromillions trekking bekijken.");
+            SchrijfLog("6. Een account genereren");
+            SchrijfLog("7. Een iban controleren");
+            SchrijfLog("8. Faculteit berekenen");
+            SchrijfLog("9. Rij van fibonaci printen");
+            SchrijfLog("10. Programma stoppen");
+            SchrijfLog("Geef een nummer in tussen 1 en 8.");
         }
         static void Main(string[] args)
         {
-            // Console.WriteLine(runMathFunctions());
-            // Console.WriteLine("random number = " + RandomNr());
-            // Console.WriteLine("random number with range 100 = " + RandomNr(100));
-
-            // LottoTrekking();
-            // EuroMillions();
-
-            /* try
-            {
-                SchrijfLog(AccountGenerator(args[0], args[1], args[2]));
-            }
-            catch (System.IndexOutOfRangeException){
-                SchrijfLog("Voor de account generator moeten 3 argumenten meegegeven worden!");
-            }
-            catch (System.Exception)
-            {
-                SchrijfLog("er is een probleem");
-            }*/
-
-            try
-            {
-                CheckIban("BE07 7390 1412 0066");
-            }
-            catch (Exception e)
-            {
-                SchrijfLog(e.Message);
-            }
+            RunMenu();
+            int input = Convert.ToInt32(Console.ReadLine());
+            do{
+                switch (input)
+                {
+                    case 1:
+                        Console.BackgroundColor = ConsoleColor.Blue;
+                        SchrijfLog(RunMathFunctions());
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        break;
+                    case 2:
+                        Console.BackgroundColor = ConsoleColor.Blue;
+                        SchrijfLog(RandomNr());
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        break;
+                    case 3:
+                        Console.BackgroundColor = ConsoleColor.Blue;
+                        SchrijfLog("Wat is de range?");
+                        int range = Convert.ToInt32(Console.ReadLine());
+                        SchrijfLog(RandomNr(range));
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        break;
+                    case 4:
+                        Console.BackgroundColor = ConsoleColor.Blue;
+                        LottoTrekking();
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        break;
+                    case 5:
+                        Console.BackgroundColor = ConsoleColor.Blue;
+                        EuroMillions();
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        break;
+                    case 6:
+                        Console.BackgroundColor = ConsoleColor.Blue;
+                        try
+                        {
+                            SchrijfLog(AccountGenerator(args[0], args[1], args[2]));
+                        }
+                        catch (System.IndexOutOfRangeException){
+                            SchrijfLog("Voor de account generator moeten 3 argumenten meegegeven worden!");
+                        }
+                        catch (System.Exception)
+                        {
+                            SchrijfLog("er is een probleem");
+                        }
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        break;
+                    case 7:
+                        Console.BackgroundColor = ConsoleColor.Blue;
+                        try
+                        {
+                            CheckIban("BE07 7390 1412 0066");
+                        }
+                        catch (Exception e)
+                        {
+                            SchrijfLog(e.Message);
+                        }
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        break;
+                    case 8:
+                        Console.BackgroundColor = ConsoleColor.Blue;
+                        SchrijfLog("Van welk getal wil de faculteit berekenen?");
+                        int lengte = Convert.ToInt32(Console.ReadLine());
+                        SchrijfLog(Faculteit(lengte));
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        break;
+                    case 9:
+                        Console.BackgroundColor = ConsoleColor.Blue;
+                        SchrijfLog("Hoeveel getallen wil je in de rij van Fibonacci?");
+                        int fib = Convert.ToInt32(Console.ReadLine());
+                        SchrijfLog(Fibonacci(fib));
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        SchrijfLog("het nummer ligt niet tussen 1 en 8!");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+                }
+                RunMenu();
+                input = Convert.ToInt32(Console.ReadLine());
+            }while(input != 10);
+            SchrijfLog("Programma gestopt");
         }
     }
 }
