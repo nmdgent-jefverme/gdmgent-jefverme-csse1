4abd226490c268bd110c55c12b38bb29e1c2b326
nmdgent-jefverme <jefvermeireown@gmail.com>
2019-11-06 11:56:59 +0100

    wip

--- opdrachten/Opdracht05/Program.cs
+++ opdrachten/Opdracht05/Program.cs
@@ -1,52 +1,155 @@
 ﻿using System;
+using System.Threading;
+
 
 namespace Opdracht05
 {
     class Program
     {
+        static void SchrijfLog(object input){ Console.WriteLine(input.ToString()); }
         static Random rnd = new Random();
-        static int som(int a, int b){
+        static int Som(int a, int b){
             return a + b;
         }
-        static int verschil(int a, int b){
+        static int Verschil(int a, int b){
             return a - b;
         }
-        static double quotient(double a, double b){
+        static double Quotient(double a, double b){
             return a/b;
         }
-        static int product(int a, int b){
+        static int Product(int a, int b){
             return a * b;
         }
-        static double modulo(double a, double b){
+        static double Modulo(double a, double b){
             return a % b;
         }
-        static int verhogen(int a){
+        static int Verhogen(int a){
             return a + 1;
         }
-        static int verlagen(int a){
+        static int Verlagen(int a){
             return a - 1;
         }
-        static string runMathFunctions(){
-            string output = "5 + 6 = " + som(5,6) + '\n';
-            output += "8 - 5 = " + verschil(8,5) + '\n';
-            output += "8 / 4 = " + quotient(8,4) + '\n';
-            output += "5 * 3 = " + product(5,3) + '\n';
-            output += "7 verhogen = " + verhogen(7) + '\n';
-            output += "8 verhogen = " + verhogen(8) + '\n';
+        static string RunMathFunctions(){
+            string output = "5 + 6 = " + Som(5,6) + '\n';
+            output += "8 - 5 = " + Verschil(8,5) + '\n';
+            output += "8 / 4 = " + Quotient(8,4) + '\n';
+            output += "5 * 3 = " + Product(5,3) + '\n';
+            output += "7 verhogen = " + Verhogen(7) + '\n';
+            output += "8 verhogen = " + Verlagen(8) + '\n';
             return output;
         }
-        static int randomNr(int a = 0){
-            if(a == 0){
+        static int RandomNr(int range = 0){
+            if(range == 0){
                 return rnd.Next();
             } else {
-                return rnd.Next(a);
+                return rnd.Next(1, range);
             }
+        }
+        static int[] GenerateNumberArray(int length, int range){
+            int[] nrArray = new int[length];
+            for (int i = 0; i < nrArray.Length; i++)
+            {
+                int nr = RandomNr(range);
+                int pos = Array.IndexOf(nrArray, nr);
+                while (pos != -1)
+                {
+                    nr = RandomNr(range);
+                    pos = Array.IndexOf(nrArray, nr);
+                }
+                nrArray[i] = nr;
+            }
+            return nrArray;
+        }
+        static void PrintArray(int[] input, int range){
+            for (int i = 0; i < input.Length; i++)
+            {
+                int sleepTimer = 100;
+                for (int j = 0; j < 7; j++)
+                {
+                    Console.Write(RandomNr(range));
+                    Thread.Sleep(sleepTimer);
+                    Console.Write("\b \b");
+                    Console.Write("\b \b");
+                    sleepTimer += 50;
+                }
+                Console.WriteLine(input[i]);
+            }
+        }
+        static void LottoTrekking(){
+            Console.WriteLine("De lottotrekking van deze week.");
+            int[] nrArray = GenerateNumberArray(6, 45);
+            Console.WriteLine("De winnende nummers zijn:");
+            PrintArray(nrArray, 45);
+        }
+        static void EuroMillions(){
+            Console.WriteLine("De euromillions trekking van deze week.");
+            int[] nrArray = GenerateNumberArray(5, 50);
+            Console.WriteLine("De winnende nummers zijn:");
+            PrintArray(nrArray, 50);
+            int[] starArray = GenerateNumberArray(2, 12);
+            Console.WriteLine("De winnende sterren zijn:");
+            PrintArray(starArray, 12);
+        }
+
+        static string AccountGenerator(string role, string fName, string lName){
+            string email = "";
+            string fullName = "";
+            if(role.ToLower() == "docent"){
+                if (fName.Length < 4){
+                    fullName += fName.ToLower();
+                } else {
+                    fullName += fName.ToLower().Substring(0, 4) + lName.ToLower().Substring(0, 2);
+                }
+                email = "@arteveldehs.be";
+            } else if(role.ToLower() == "student"){
+                if (fName.Length < 4){
+                    fullName += fName.ToLower() + lName.ToLower().Substring(0, 4);
+                } else {
+                    fullName += fName.ToLower().Substring(0, 4) + lName.ToLower().Substring(0, 4);
+                }
+                email = "@student.arteveldehs.be";
+            }
+            return fullName + email;
+        }
+        static void CheckIban(string iban){
+            string str = iban.Replace(" ", "");
+            SchrijfLog(str.Length);
+            if (str.Length != 16){
+                throw new System.ArgumentException("De lengte van een iban moet precies 16 karakters zijn.");
+            }
+            string numbers = str.Substring(4);
+            double modulo = Modulo(Convert.ToDouble(numbers), 97.0);
+            SchrijfLog(modulo);
         }
         static void Main(string[] args)
         {
             // Console.WriteLine(runMathFunctions());
-            Console.WriteLine("random number = " + randomNr());
-            Console.WriteLine("random number with range 100 = " + randomNr(100));
+            // Console.WriteLine("random number = " + RandomNr());
+            // Console.WriteLine("random number with range 100 = " + RandomNr(100));
+
+            // LottoTrekking();
+            // EuroMillions();
+
+            /* try
+            {
+                SchrijfLog(AccountGenerator(args[0], args[1], args[2]));
+            }
+            catch (System.IndexOutOfRangeException){
+                SchrijfLog("Voor de account generator moeten 3 argumenten meegegeven worden!");
+            }
+            catch (System.Exception)
+            {
+                SchrijfLog("er is een probleem");
+            }*/
+
+            try
+            {
+                CheckIban("BE07 7390 1412 0066");
+            }
+            catch (Exception e)
+            {
+                SchrijfLog(e.Message);
+            }
         }
     }
 }
